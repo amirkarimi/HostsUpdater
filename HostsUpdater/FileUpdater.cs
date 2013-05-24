f019d117619f9fb1e3e6b5fd60c1bae057ab8264
Amir Karimi <a.karimi.k@gmail.com>
2013-05-24 16:41:41 +0430

    Add first version of the source

---
+++ HostsUpdater/FileUpdater.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace HostsUpdater
+{
+    public class FileUpdater
+    {
+        private readonly string fileName;
+        private IList<string> lines;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FileUpdater"/> class.
+        /// </summary>
+        /// <param name="fileName">Name of the file.</param>
+        public FileUpdater(string fileName)
+        {
+            this.fileName = fileName;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FileUpdater"/> class.
+        /// </summary>
+        public FileUpdater()
+        {
+            this.fileName = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.System), @"drivers\etc\hosts");
+        }
+
+        /// <summary>
+        /// Reads this instance.
+        /// </summary>
+        private void Read()
+        {
+            this.lines = File.ReadAllLines(this.fileName).ToList();
+        }
+
+        /// <summary>
+        /// Writes this instance.
+        /// </summary>
+        private void Write()
+        {
+            File.WriteAllLines(this.fileName, this.lines);
+        }
+
+        /// <summary>
+        /// Updates the host.
+        /// </summary>
+        /// <param name="ip">The IP address.</param>
+        /// <param name="name">The name.</param>
+        public void UpdateHostRecord(string ip, string name)
+        {
+            this.Read();
+
+            name = name.Trim();
+            ip = ip.Trim();
+
+            bool updated = false;
+            int count = this.lines.Count;
+            for (int i = 0; i < count; i++)
+            {
+                var line = this.lines[i];
+
+                if (line.StartsWith("#"))
+                {
+                    // Comment
+                    continue;
+                }
+
+                var splited = line.Split(new char[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries);
+                if (splited.Length < 2)
+                {
+                    // Incorrect format
+                    continue;
+                }
+
+                if (splited[1].Trim().Equals(name, StringComparison.InvariantCultureIgnoreCase))
+                {
+                    // Update
+                    this.lines[i] = this.GetLine(ip, splited[1]);
+                    updated = true;
+                    break;
+                }
+            }
+
+            if (!updated)
+            {
+                // Insert
+                this.lines.Add(this.GetLine(ip, name));
+            }
+
+            this.Write();
+        }
+
+        /// <summary>
+        /// Gets the line.
+        /// </summary>
+        /// <param name="ip">The IP.</param>
+        /// <param name="splited">The name.</param>
+        /// <returns></returns>
+        private string GetLine(string ip, string name)
+        {
+            return string.Format("{0}\t\t\t{1}", ip, name);
+        }
+    }
+}
---
+++ HostsUpdater/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HostsUpdater
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine();
+            Console.WriteLine("HostsUpdater (C) 2013 by Amir Karimi | www.dev-frame.com");
+            Console.ResetColor();
+
+            if (args.Length < 2)
+            {
+                Console.WriteLine("Usage: HostsUpdater [ip-address] [name]");
+                return;
+            }
+
+            var updater = new FileUpdater();
+            updater.UpdateHostRecord(args[0], args[1]);
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Host file has been updated successfully.");
+
+            Console.ResetColor();
+        }
+    }
+}
